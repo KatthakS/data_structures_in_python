634ae1dd8b64f42be847e610a49f52a3998f0e11
Katthak <kshah37@horizon.csueastbay.edu>
2020-10-09 20:33:29 -0700

    merge two sorted arrays as one sorted array

---
+++ merge_sorted_arrays.py
@@ -0,0 +1,40 @@
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+"""
+Constraints:
+    * -10^9 <= nums1[i], nums2[i] <= 10^9
+    * nums1.length == m + n
+    * nums2.length == n
+"""
+
+from typing import List
+
+class MergeSortedArrays:
+    def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        l1 = len(nums1) - 1
+        
+        m-=1
+        n-=1
+        #for i in range(len(nums1), 0, -1):
+        while (m >= 0) and (n >= 0):
+            if nums1[m] >= nums2[n]:
+                # move nums1 element to the last available positioin at nums1
+                nums1[l1] = nums1[m]
+                m -= 1
+            else:
+                nums1[l1] = nums2[n]
+                n -= 1
+            l1 -= 1
+        
+        while n >= 0:
+            nums1[l1] = nums2[n]
+            n-=1
+            l1-=1
+
+    nums1 = [2, 2, 3, 0, 0, 0]
+    nums2 = [1, 5, 6]
+    merge(nums1, 3, nums2, 3)   
+    print (nums1)     
